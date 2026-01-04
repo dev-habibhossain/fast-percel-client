d223361c3aaaac46ba965a538a6e6fb3cf4f7092
Habib Hossain <habibfahim71@gmail.com>
2026-01-04 23:58:24 +0600

    font setup done

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,10 @@ import {
 } from "react-router";
 import { router } from './router/router';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <div className="font-urbanist">
+      <RouterProvider router={router} />
+    </div>
+  </StrictMode>
+);
